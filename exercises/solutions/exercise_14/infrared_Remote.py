b90e54aed231de7c863578cb0a7cf864efd43785
Uli Raich <uli.raich@gmail.com>
2021-05-06 17:43:43 +0200

    name change of program and slight change, adding a print statement showing it is alive

--- exercises/solutions/exercise_14/infrared_Remote.py
+++ exercises/solutions/exercise_14/infrared_Remote.py
@@ -76,6 +76,8 @@ IR_code = {0xff6897 : "0", 0xff30cf : "1", 0xff18e7 : "2", 0xff7a85 : "3", 0xff1
            0xff38c7 : "5", 0xff5aa5 : "6", 0xff42bd : "7", 0xff4ab5 : "8", 0xff52ad : "9",
            0xffa25d : "CH-", 0xff629d : "CH", 0xffe21d : "CH+", 0xff22dd : "|<<", 0xff02fd : "|>>", 0xffc23d : ">||",
            0xffe01f: "-", 0xffa857 : "+", 0xff906f : "EQ", 0xff9867 : "100+", 0xffb04f : "200+"}
+
+print("Ready to receive IR commands")
 while True:
     irValue = ir.read()
     if irValue:
