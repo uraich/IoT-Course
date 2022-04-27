e07db475a98b278198a390ac106aa713425a830f
uraich <uli.raich@gmail.com>
2022-04-27 17:58:47 +0200

    corrected i2cScan.py to print only 128 addresses

--- exercises/solutions/i2c/i2cScan.py
+++ exercises/solutions/i2c/i2cScan.py
@@ -32,7 +32,7 @@ addr = i2c.scan()
 print("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f")
 
 j=0
-for i in range (0,16):
+for i in range (0,8):
     print('%02x'%(16*i),end=': ')
     for j in range(0,16):
         if 16*i+j in addr:
