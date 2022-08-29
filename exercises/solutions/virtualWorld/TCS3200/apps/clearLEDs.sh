5cccb14f9e163f486d89c03bbfbe7b477817f062
uraich <uli.raich@gmail.com>
2022-08-29 15:00:17 +0200

    removed double virtual world

--- exercises/solutions/virtualWorld/TCS3200/apps/clearLEDs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ampy run clearLEDs.py
--- exercises/solutions/virtualWorld/vpython/cylinder3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# Create a first 3d object with VPython
-# import the necessary methods
-from vpython import *
-# create a cylinder
-cylinder(pos=vector(-5,0,0),axis=vector(4,0,0), radius = 0.5)
-sphere(radius=0.2, color=color.red)
