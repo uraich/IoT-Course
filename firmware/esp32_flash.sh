a3cdcef99fbc9b12aa5d939c00e24291da951ada
uraich <uli.raich@gmail.com>
2022-10-05 16:54:27 +0200

    added baud rate to speed up flashing

--- firmware/esp32_flash.sh
+++ firmware/esp32_flash.sh
@@ -2,4 +2,4 @@
 # flashes the esp32 camera board with MicroPython
 # written by U. Raich for the AIS conference 2020 Kinshasa
 # make sure GPIO-0 is connected to ground through a jumper
-esptool write_flash --flash_size=detect --flash_mode dio 0x1000 firmware.bin
+esptool -b 460800 write_flash --flash_size=detect --flash_mode dio 0x1000 firmware.bin
