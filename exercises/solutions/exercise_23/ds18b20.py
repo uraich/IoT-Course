a97e874ba0ab9738dd420dadf3e5029745305138
Uli Raich <uli.raich@gmail.com>
2026-01-16 10:49:32 +0100

    modified ds18b20 demo to work on esp32-wroom. (Changed the GPIO number of the data line

--- exercises/solutions/exercise_23/ds18b20.py
+++ exercises/solutions/exercise_23/ds18b20.py
@@ -3,7 +3,8 @@ import machine
 import onewire, ds18x20
 
 # the device is on GPIO12
-dat = machine.Pin(35)  # this corresponds to D2
+# dat = machine.Pin(35)  # this corresponds to D2 on esp32s3-mini
+dat = machine.Pin(21)  # this corresponds to D2 on esp32-wroom
 
 # create the onewire object
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
